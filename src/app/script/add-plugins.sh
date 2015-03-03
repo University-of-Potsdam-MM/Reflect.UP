ab42a4a5734d22f898eaac58e853820b7ade4fcd
Hendrik Geßner <HENGE01@gmail.com>
2015-03-03 03:15:57 +0100

    Adresses #1 - Removed unneccessary plugin.

--- src/app/script/add-plugins.sh
+++ src/app/script/add-plugins.sh
@@ -1,3 +1,2 @@
 phonegap plugin add https://github.com/phonegap-build/PushPlugin.git
 phonegap plugin add https://github.com/apache/cordova-plugin-device.git
-phonegap plugin add https://github.com/katzer/cordova-plugin-local-notifications.git
