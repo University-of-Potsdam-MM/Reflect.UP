f00863f0529fa25644ee39917ecf96de593554a4
Alexander Kiy <alexander.kiy@uni-potsdam.de>
2016-09-21 13:37:12 +0200

    fixed Statusbar and now checking OnlineStatus

--- www/js/index.js
+++ www/js/index.js
@@ -3,17 +3,30 @@ var Appointments = new AppointmentCollection();
 var Questions = new QuestionContainerList();
 var Config = new Configuration({id: 1});
 
-
 var app = {
     // Application Constructor
     initialize: function() {
-
         this.bindEvents();
+    },
+
+    onDeviceOffline: function(){
+        app.receivedEvent('onDeviceOffline');
+        alert('Please check your internet connection and restart!');
+        navigator.app.exitApp();
+    },
+
+    onDeviceOnline: function(){
+        app.receivedEvent('onDeviceOnline');
+        //PushServiceRegister();
+        // hide splashscreen
+        setTimeout(function() {
+            navigator.splashscreen.hide();
+        }, 2000);
 
         var router = new Router();
         Backbone.history.start();
-
     },
+
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
@@ -27,10 +40,18 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        PushServiceRegister();
-        // hide splashscreen
-        navigator.splashscreen.hide();
+
+        //document.addEventListener("offline", app.onDeviceOffline(), false);
+        //document.addEventListener("online", app.onDeviceOnline(), false);
+
+        if((navigator.network.connection.type).toUpperCase() != "NONE" && (navigator.network.connection.type).toUpperCase() != "UNKNOWN") {
+            app.onDeviceOnline();
+        }else{
+            app.onDeviceOffline();
+        }
+
     },
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         console.log('Received Event: ' + id);
