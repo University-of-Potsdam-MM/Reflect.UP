c8e178130bb62bfbe990f3f7e1fa21a1a9dcd32e
Alexander Kiy <alekiy@uni-potsdam.de>
2018-11-12 11:10:21 +0100

    removed tou from info page

--- src/pages/info/info.ts
+++ src/pages/info/info.ts
@@ -8,13 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class InfoPage {
 
-  showTOS = false;
-
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
-  toggleTOS() {
-    this.showTOS = !this.showTOS;
-  }
-
 }
